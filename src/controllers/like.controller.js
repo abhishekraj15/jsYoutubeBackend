9a52c50494b6629d4fa94882b5203ed562482a50
Vanshika Rajput <vanshikavce19@gmail.com>
2024-12-03 22:26:38 +0530

    Get liked Video

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -58,7 +58,77 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
 // });
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-  
+  const { page = 1, limit = 10 } = req.query;
+
+  const skip = (page - 1) * limit;
+
+  const matchCondition = {
+    likedBy: new mongoose.Types.ObjectId(req?.user?._id),
+  };
+
+  const likedVideo = await Like.aggregate([
+    {
+      $match: matchCondition,
+    },
+
+    //video details
+    {
+      $lookup: {
+        from: "videos",
+        localField: "video",
+        foreignField: "_id",
+        as: "videoDetails",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "ownerDetails",
+            },
+          },
+          {
+            $project: {
+              title: 1,
+              description: 1,
+              views: 1,
+              duration: 1,
+              "thumbnail.url": 1,
+              "videoFile.url": 1,
+              createdAt: 1,
+              ownerDetails: {
+                username: 1,
+                avatar: 1,
+              },
+            },
+          },
+        ],
+      },
+    },
+    { $unwind: "$videoDetails" },
+    { $skip: skip },
+    { $limit: parseInt(limit, 10) },
+    {
+      $project: {
+        videoDetails: 1,
+        createdAt: 1,
+      },
+    },
+  ]);
+  // Count total videos for pagination
+  const totalVideos = await Like.countDocuments(matchCondition);
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        likedVideo,
+        totalVideos,
+        totalPages: Math.ceil(totalVideos / limit),
+        currentPage: parseInt(page, 10),
+      },
+      "Videos fetched successfully"
+    )
+  );
 });
 
 export {
