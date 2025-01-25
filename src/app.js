dc0910d19a10e1d812b540ec6ad71706322cd2b8
abhishekraj15 <rabhi0766@gmail.com>
2025-01-25 18:45:30 +0530

    comment added

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
-// routes import
+// routes import part
 import userRouter from "./routes/user.routes.js";
 import healthcheckRouter from "./routes/healthcheck.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
@@ -38,7 +38,7 @@ import likeRouter from "./routes/like.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import dashboardRouter from "./routes/dashboard.routes.js";
 
-//route decleartion
+//route decleartion part
 app.use("/api/v1/users", userRouter);
 
 app.use("/api/v1/healthcheck", healthcheckRouter);
